b6919ace839a06f71de00eb35b56e0a4246929ac
tomyItSky <tomy.agung@skyparking.co.id>
2024-08-18 23:15:31 +0700

    .

--- src/api/apiClient.js
+++ src/api/apiClient.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 export const apiClient = axios.create({
-  //   baseURL: "http://localhost:3008",
-  baseURL: "https://dev-valetapi.skyparking.online",
+  baseURL: process.env.REACT_APP_BASE_URL || "http://localhost:3008",
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
